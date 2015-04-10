e8edb5ced94d1b84156ee6c63b0b071cd5114ba5
Riandy Rahman Nugraha <riandyrn@gmail.com>
2015-04-10 11:57:20 +0700

    first push

---
+++ src/LU.java
@@ -0,0 +1,15 @@
+
+public class LU {
+
+	/*
+	 * Kelas ini bertugas untuk menlakukan konversi
+	 * dari kalimat yang didapat dari ASR ke bentuk
+	 * frame
+	 */
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello world!");
+	}
+	
+}
